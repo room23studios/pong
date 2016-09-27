c9784d4e2644b2d82669df3d96b87ff7440d26a4
Haker53535 <haker53535jakub@gmail.com>
2016-09-27 17:41:39 +0200

    Style changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,48 +5,60 @@ using namespace std;
 using namespace sf;
 
 int main(){
+
   char x;
   unsigned short port;
   string ipString;
 
   TcpListener listener;
-  TcpSocket client;
   TcpSocket socket;
   IpAddress address;
+  TcpSocket client;
 
-  cout<<"Hello, there. Welcome in pong. Would you like to host server? y/n" <<endl;
-  cin>>x;
-  if(x == 'y'){
-    cout<<"What port do you want to host on?" <<endl;
-    cin>>port;
+  cout << "Hello, there. Welcome in pong. Would you like to host server? y/n" <<endl;
+  cin >> x;
+
+  if (x == 'y'){
+    cout << "What port do you want to host on?" << endl;
+    cin >> port;
+
     address = IpAddress::getLocalAddress();
-    cout<<"Hosting on: " << address.toString() << ":" << port <<endl;
+
+    cout << "Hosting on: " << address.toString() << ":" << port <<endl;
+
     if (listener.listen(25565) != sf::Socket::Done)
     {
-      cout<<"Nobody connect"<<endl;;
+      cout << "Nobody connect" << endl;;
       return 0;
     }
 
     if (listener.accept(client) != sf::Socket::Done)
     {
-      cout<<"Problem here";
+      cout << "Problem here";
       return 0;
     }
-    cout<<"Client connected";
-  }
-  else{
-    cout<<"What ip should I connect to?"<<endl;
-    cin>>ipString;
+
+    cout << "Client connected";
+
+    }
+
+  else {
+    cout << "What ip should I connect to?" << endl;
+    cin >> ipString;
+
     address = IpAddress(ipString);
-    cout<<"What port should I connect to?"<<endl;
-    cin>>port;
-    cout<<"Connecting to: " <<  address.toString() << ":" << port <<" ..."<<endl;
+
+    cout << "What port should I connect to?" << endl;
+    cin >> port;
+    cout << "Connecting to: " << address.toString() << ":" << port << " ..." << endl;
+
     Socket::Status status = socket.connect(address, port);
-    if(status != Socket::Done)
+
+    if (status != Socket::Done)
     {
-      cout<<"Cannot connect"<<endl;;
+      cout << "Cannot connect" << endl;;
       return 0;
     }
-    cout<<"Connected"<<endl;;
+    cout << "Connected" << endl;;
   }
 }
