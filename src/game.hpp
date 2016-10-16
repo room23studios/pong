5aa355fea879ec5415971f0ea2837bad07a00032
Marcel Guzik <mguzik555@gmail.com>
2016-10-16 17:41:11 +0200

    Added ball movement - closes #13

--- src/game.hpp
+++ src/game.hpp
@@ -1,17 +1,20 @@
 #include <SFML/Graphics.hpp>
 
+#include "ball.hpp"
+
 using namespace sf;
 
 class Game {
 private:
 	RenderWindow window;
 
-	CircleShape ball;
+	Ball ball;
 	RectangleShape player1;
 	RectangleShape player2;
 
 	const int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480;
 	const float BALL_RADIUS = 10, PADDLE_WIDTH = 10, PADDLE_HEIGHT = 50;
+	const unsigned int MAX_FPS = 60;
 
 public:
 	Game();
